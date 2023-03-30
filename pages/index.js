3733bbbb3d4fdcf61582cd21364f2f0baa13a4c8
Lee Watson <watsonalee@gmail.com>
2023-03-30 20:40:00 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import ContactForm from '@/components/ContactForm'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home(){
+    return (
+    <>
+    <Head>
+        <title>Code Lancashire</title>
+    </Head>
+    <div classname="max-w-4xl mx auto p-5">
+        <ContactForm />
+    </div>
+    </>
+    )
+}
